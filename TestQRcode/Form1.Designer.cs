9886dc4f1c42f86829723179edfd492aff0e0aa9
tovatron <darknjght2001@gmail.com>
2023-04-28 05:21:35 +0700

    Many change
    Details data and short data
    Export to Excel
    ...

--- TestQRcode/Form1.Designer.cs
+++ TestQRcode/Form1.Designer.cs
@@ -32,23 +32,50 @@ namespace TestQRcode
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle15 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btnStart = new System.Windows.Forms.Button();
             this.cboDevice = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.bntStop = new System.Windows.Forms.Button();
             this.colQRCodeNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColProductName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColWeight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bntStop = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.ColqrCodeNumber1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColproductName1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Coltype1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Colweight1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Colstats1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Coleitime1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Colstime1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(254, 25);
+            this.btnStart.Location = new System.Drawing.Point(260, 17);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 0;
@@ -60,7 +87,7 @@ namespace TestQRcode
             // 
             this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboDevice.FormattingEnabled = true;
-            this.cboDevice.Location = new System.Drawing.Point(93, 25);
+            this.cboDevice.Location = new System.Drawing.Point(90, 19);
             this.cboDevice.Name = "cboDevice";
             this.cboDevice.Size = new System.Drawing.Size(155, 21);
             this.cboDevice.TabIndex = 1;
@@ -68,7 +95,7 @@ namespace TestQRcode
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 28);
+            this.label1.Location = new System.Drawing.Point(6, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 13);
             this.label1.TabIndex = 2;
@@ -77,7 +104,7 @@ namespace TestQRcode
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 52);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 59);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(236, 233);
             this.pictureBox1.TabIndex = 3;
@@ -95,15 +122,44 @@ namespace TestQRcode
             this.colQRCodeNumber,
             this.ColProductName,
             this.ColWeight});
-            this.dataGridView1.Location = new System.Drawing.Point(750, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(534, 6);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(434, 333);
             this.dataGridView1.TabIndex = 5;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // colQRCodeNumber
+            // 
+            this.colQRCodeNumber.DataPropertyName = "Number";
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.colQRCodeNumber.DefaultCellStyle = dataGridViewCellStyle11;
+            this.colQRCodeNumber.HeaderText = "Mã code";
+            this.colQRCodeNumber.Name = "colQRCodeNumber";
+            this.colQRCodeNumber.ReadOnly = true;
+            this.colQRCodeNumber.Width = 130;
+            // 
+            // ColProductName
+            // 
+            this.ColProductName.DataPropertyName = "Productname";
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.ColProductName.DefaultCellStyle = dataGridViewCellStyle12;
+            this.ColProductName.HeaderText = "Tên sản phẩm";
+            this.ColProductName.Name = "ColProductName";
+            this.ColProductName.ReadOnly = true;
+            this.ColProductName.Width = 160;
+            // 
+            // ColWeight
+            // 
+            this.ColWeight.DataPropertyName = "Type";
+            dataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.ColWeight.DefaultCellStyle = dataGridViewCellStyle13;
+            this.ColWeight.HeaderText = "Type";
+            this.ColWeight.Name = "ColWeight";
+            this.ColWeight.ReadOnly = true;
             // 
             // bntStop
             // 
-            this.bntStop.Location = new System.Drawing.Point(344, 25);
+            this.bntStop.Location = new System.Drawing.Point(351, 17);
             this.bntStop.Name = "bntStop";
             this.bntStop.Size = new System.Drawing.Size(75, 23);
             this.bntStop.TabIndex = 6;
@@ -111,37 +167,156 @@ namespace TestQRcode
             this.bntStop.UseVisualStyleBackColor = true;
             this.bntStop.Click += new System.EventHandler(this.bntStop_Click);
             // 
-            // colQRCodeNumber
+            // tabControl1
             // 
-            this.colQRCodeNumber.DataPropertyName = "Number";
-            this.colQRCodeNumber.HeaderText = "Mã code";
-            this.colQRCodeNumber.Name = "colQRCodeNumber";
-            this.colQRCodeNumber.Width = 130;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1092, 565);
+            this.tabControl1.TabIndex = 7;
             // 
-            // ColProductName
+            // tabPage1
             // 
-            this.ColProductName.DataPropertyName = "Productname";
-            this.ColProductName.HeaderText = "Tên sản phẩm";
-            this.ColProductName.Name = "ColProductName";
-            this.ColProductName.Width = 160;
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.bntStop);
+            this.tabPage1.Controls.Add(this.cboDevice);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.btnStart);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1084, 539);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Main";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // ColWeight
+            // tabPage2
             // 
-            this.ColWeight.DataPropertyName = "Type";
-            this.ColWeight.HeaderText = "Type";
-            this.ColWeight.Name = "ColWeight";
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1084, 539);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Data";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(974, 490);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "&Reset data";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(974, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColqrCodeNumber1,
+            this.ColproductName1,
+            this.Coltype1,
+            this.Colweight1,
+            this.Colstats1,
+            this.Coleitime1,
+            this.Colstime1});
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(943, 539);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // ColqrCodeNumber1
+            // 
+            this.ColqrCodeNumber1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.ColqrCodeNumber1.DataPropertyName = "Number";
+            dataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.ColqrCodeNumber1.DefaultCellStyle = dataGridViewCellStyle14;
+            this.ColqrCodeNumber1.HeaderText = "Mã code";
+            this.ColqrCodeNumber1.Name = "ColqrCodeNumber1";
+            this.ColqrCodeNumber1.ReadOnly = true;
+            // 
+            // ColproductName1
+            // 
+            this.ColproductName1.DataPropertyName = "productName";
+            dataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.ColproductName1.DefaultCellStyle = dataGridViewCellStyle15;
+            this.ColproductName1.HeaderText = "Tên sản phẩm";
+            this.ColproductName1.Name = "ColproductName1";
+            this.ColproductName1.ReadOnly = true;
+            this.ColproductName1.Width = 150;
+            // 
+            // Coltype1
+            // 
+            this.Coltype1.DataPropertyName = "type";
+            dataGridViewCellStyle16.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Coltype1.DefaultCellStyle = dataGridViewCellStyle16;
+            this.Coltype1.HeaderText = "Loại sản phẩm";
+            this.Coltype1.Name = "Coltype1";
+            this.Coltype1.ReadOnly = true;
+            this.Coltype1.Width = 120;
+            // 
+            // Colweight1
+            // 
+            this.Colweight1.DataPropertyName = "weight";
+            dataGridViewCellStyle17.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
+            this.Colweight1.DefaultCellStyle = dataGridViewCellStyle17;
+            this.Colweight1.HeaderText = "Cân nặng";
+            this.Colweight1.Name = "Colweight1";
+            this.Colweight1.ReadOnly = true;
+            this.Colweight1.Width = 90;
+            // 
+            // Colstats1
+            // 
+            this.Colstats1.DataPropertyName = "stats";
+            dataGridViewCellStyle18.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Colstats1.DefaultCellStyle = dataGridViewCellStyle18;
+            this.Colstats1.HeaderText = "Tình trạng";
+            this.Colstats1.Name = "Colstats1";
+            this.Colstats1.ReadOnly = true;
+            // 
+            // Coleitime1
+            // 
+            this.Coleitime1.DataPropertyName = "eitime";
+            dataGridViewCellStyle19.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Coleitime1.DefaultCellStyle = dataGridViewCellStyle19;
+            this.Coleitime1.HeaderText = "Thời gian";
+            this.Coleitime1.Name = "Coleitime1";
+            this.Coleitime1.ReadOnly = true;
+            this.Coleitime1.Width = 170;
+            // 
+            // Colstime1
+            // 
+            this.Colstime1.DataPropertyName = "stime";
+            dataGridViewCellStyle20.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.Colstime1.DefaultCellStyle = dataGridViewCellStyle20;
+            this.Colstime1.HeaderText = "Lưu trữ";
+            this.Colstime1.Name = "Colstime1";
+            this.Colstime1.ReadOnly = true;
+            this.Colstime1.Width = 170;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1196, 585);
-            this.Controls.Add(this.bntStop);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.cboDevice);
-            this.Controls.Add(this.btnStart);
+            this.ClientSize = new System.Drawing.Size(1095, 566);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -149,15 +324,15 @@ namespace TestQRcode
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
 
         #endregion
 
@@ -167,10 +342,23 @@ namespace TestQRcode
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private Button bntStop;
+        private System.Windows.Forms.Button bntStop;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
         private DataGridViewTextBoxColumn colQRCodeNumber;
         private DataGridViewTextBoxColumn ColProductName;
         private DataGridViewTextBoxColumn ColWeight;
+        private DataGridViewTextBoxColumn ColqrCodeNumber1;
+        private DataGridViewTextBoxColumn ColproductName1;
+        private DataGridViewTextBoxColumn Coltype1;
+        private DataGridViewTextBoxColumn Colweight1;
+        private DataGridViewTextBoxColumn Colstats1;
+        private DataGridViewTextBoxColumn Coleitime1;
+        private DataGridViewTextBoxColumn Colstime1;
     }
 }
 
--- TestQRcode/Form1.cs
+++ TestQRcode/Form1.cs
@@ -10,7 +10,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
+using Microsoft.Office.Interop.Excel;
 using ZXing;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace TestQRcode
 {
@@ -20,12 +22,12 @@ namespace TestQRcode
         {
             InitializeComponent();
             dataGridView1.AutoGenerateColumns = false;
+            dataGridView2.AutoGenerateColumns = false;
         }
 
         FilterInfoCollection filterInfoCollection;
         VideoCaptureDevice captureDevice;
         List<ThongtinQR> listQR = new List<ThongtinQR>();
-
         private void Form1_Load(object sender, EventArgs e)
         {
             filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
@@ -43,9 +45,17 @@ namespace TestQRcode
         }
         private void btnStart_Click(object sender, EventArgs e)
         {
-            captureDevice = new VideoCaptureDevice(filterInfoCollection[cboDevice.SelectedIndex].MonikerString);
-            captureDevice.NewFrame += CaptureDevice_NewFrame;
-            StartCapturing();
+            try
+            {
+                captureDevice = new VideoCaptureDevice(filterInfoCollection[cboDevice.SelectedIndex].MonikerString);
+                captureDevice.NewFrame += CaptureDevice_NewFrame;
+                StartCapturing();
+                MessageBox.Show("Camera đã được kết nối!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể kết nối Camera: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
         private void bntStop_Click(object sender, EventArgs e)
         {
@@ -64,7 +74,7 @@ namespace TestQRcode
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
-        private Bitmap CropImage(Bitmap bitmap, Rectangle cropArea)
+        private Bitmap CropImage(Bitmap bitmap, System.Drawing.Rectangle cropArea)
         {
             Bitmap croppedBitmap = bitmap.Clone(cropArea, bitmap.PixelFormat);
             return croppedBitmap;
@@ -87,14 +97,86 @@ namespace TestQRcode
             private string type;
             public string Type { get => type; set => type = value; }
 
-            public ThongtinQR(string number, string productname, string type)
+            private string weight;
+            public string Weight { get => weight; set => weight = value; }
+
+            private string stats;
+            public string Stats { get => stats; set => stats = value; }
+
+            private string eitime;
+            public string Eitime { get => eitime; set => eitime = value; }
+
+            private string stime;
+            public string Stime { get => stime; set => stime = value; }
+
+            public ThongtinQR(string number, string productname, string type, string weight, string stats, string eitime, string stime)
             {
                 Number = number;
                 Productname = productname;
                 Type = type;
+                Weight = weight;
+                Stats = stats;
+                Eitime = eitime;
+                Stime = stime;
             }
 
 
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            // Khởi tạo ứng dụng Excel và Workbook mới
+            Excel.Application excel = new Excel.Application();
+            Workbook workbook = excel.Workbooks.Add();
+
+            // Lấy Worksheet đầu tiên trong Workbook
+            Worksheet worksheet = workbook.ActiveSheet;
+
+            // Thêm header
+            worksheet.Cells[1, "A"].Value = "Mã Code";
+            worksheet.Cells[1, "B"].Value = "Tên sản phẩm";
+            worksheet.Cells[1, "C"].Value = "Thể loại";
+            worksheet.Cells[1, "D"].Value = "Cân nặng";
+            worksheet.Cells[1, "E"].Value = "Trạng thái";
+            worksheet.Cells[1, "F"].Value = "Thời gian";
+            worksheet.Cells[1, "G"].Value = "Lưu trữ";
+            Range headerRange = worksheet.Range["A1:G1"];
+            headerRange.Font.Bold = true;
+            headerRange.Font.Color = Color.Green;
+
+            // Ghi dữ liệu vào các ô
+            int row = 3;
+            foreach (ThongtinQR qr in listQR)
+            {
+                worksheet.Cells[row, "A"].Value = qr.Number;
+                worksheet.Cells[row, "B"].Value = qr.Productname;
+                worksheet.Cells[row, "C"].Value = qr.Type;
+                worksheet.Cells[row, "D"].Value = qr.Weight;
+                worksheet.Cells[row, "E"].Value = qr.Stats;
+                worksheet.Cells[row, "F"].Value = qr.Eitime;
+                worksheet.Cells[row, "G"].Value = qr.Stime;
+                row++;
+                worksheet.Columns.AutoFit();
+            }
+
+            // Hiển thị hộp thoại lưu file để cho phép người dùng chọn đường dẫn và tên file lưu trữ
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx";
+            saveFileDialog.Title = "Save Excel File";
+            saveFileDialog.ShowDialog();
+
+            // Nếu người dùng đã chọn đường dẫn và tên file lưu trữ
+            if (saveFileDialog.FileName != "")
+            {
+                // Lưu Workbook vào đường dẫn và tên file lưu trữ được chọn
+                workbook.SaveAs(saveFileDialog.FileName);
+
+                // Hiển thị thông báo lưu file thành công
+                MessageBox.Show("Excel file saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            // Đóng Workbook và Excel Application
+            workbook.Close();
+            excel.Quit();
         }
         private async void timer1_Tick(object sender, EventArgs e)
         {
@@ -108,16 +190,30 @@ namespace TestQRcode
                     if (captureDevice.IsRunning)
                         captureDevice.Stop();
                     string[] fields = result.Text.Split(';');
-                    if (fields.Length >= 3)
+                    if (fields.Length >= 7)
                     {
-                        string qrCodeNumber = fields[0];
+                        string number = fields[0];
                         string productName = fields[1];
                         string type = fields[2];
-                        ThongtinQR qr = new ThongtinQR(qrCodeNumber, productName, type);
+                        string weight = fields[3];
+                        string stats = fields[4];
+                        string eitime = fields[5];
+                        string stime = fields[6];
+                        ThongtinQR qr = new ThongtinQR(number, productName, type, weight, stats, eitime, stime);
                         listQR.Add(qr);
                         dataGridView1.DataSource = null;
                         dataGridView1.DataSource = listQR;
+
+                        dataGridView2.DataSource = null;
+                        dataGridView2.DataSource = listQR;
                     }
+                    else
+                    {
+                        if (captureDevice.IsRunning)
+                            captureDevice.Stop();
+                        MessageBox.Show("Thông tin thùng hàng bị thiếu, hãy kiểm tra lại");
+                    }
+
                     await Task.Delay(5000);
                     StartCapturing();
                 }
